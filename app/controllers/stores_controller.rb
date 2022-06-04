e0612f15858e46664be5e29a973a31f1dbe47103
Alejandro Vazquez Ibarra <alejandrovqz@kinedu.com>
2022-06-04 08:42:36 -0500

    were created the product controller and the store controller

---
+++ app/controllers/stores_controller.rb
@@ -0,0 +1,35 @@
+class StoresController < ApplicationController
+  def index
+    @stores = Store.all
+  end
+  
+  def new
+    @store = Store.new
+  end
+
+  def create
+    store = store.create(store_params)
+    store.save
+  end
+
+  def edit
+    set_store
+  end
+
+  def update
+    set_store
+    @store.update(store_params)
+  end
+
+  def destroy
+    Store.find(params[:id]).destroy
+  end
+
+  def set_store
+    @store = Store.find(params[:id])
+  end
+
+  def store_params
+    data = params.require(:store).permit(:name, :priority, :repair_products)
+  end
+end
