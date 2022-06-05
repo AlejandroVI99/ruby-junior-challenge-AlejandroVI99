9d8e129d312d942c422ad9840e30400e94c170b6
Alejandro Vazquez Ibarra <alejandrovqz@kinedu.com>
2022-06-04 23:23:07 -0500

    fix controller

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def index
+  end
+
+  def admin_view
+  end
+
+  def client_view
+  end
+end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,28 +1,56 @@
 class ProductsController < ApplicationController
+
   def index
-    @products = Product.all
+    if client_signed_in?
+      @products = Product.all
+    elsif admin_signed_in?
+      @products = Product.all
+    else
+      redirect_to '/home'
+    end
   end
+
   def new
-    @product = Product.new
+    if client_signed_in?
+      @product = Product.new
+    else
+      redirect_to '/home'
+    end
   end
+
   def create
-    product = product.create(product_params)
-    product.save
+    if client_signed_in?
+      store_id = Product.assign_store(product_params[:name])
+      @product = Product.create(
+        name: product_params[:name],
+        repair_status: 'pending',
+        client_id: current_client.id,
+        store_id: store_id
+      )
+      RepairJob.set(wait: 1.minute).perform_later(@product)
+      redirect_to products_path
+    end
   end
+
   def edit
     set_product
   end
+
   def update
     set_product
     @product.update(product_params)
+    redirect_to products_path
   end
+
   def destroy
     Product.find(params[:id]).destroy
   end
+
   def set_product
     @product = Product.find(params[:id])
   end
+
   def product_params
-    data = params.require(:product).permit(:name, :repair_price)
+    params.require(:product).permit(:name,:repair_price,:repair_status, :client_id,:store_id)
   end
 end
